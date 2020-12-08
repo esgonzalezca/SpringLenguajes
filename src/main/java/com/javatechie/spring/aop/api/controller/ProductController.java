fa5f6dcf81f86b868d58c51be7afc47b54672a1a
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2020-12-08 00:41:13 -0500

    Update ProductController.java

--- src/main/java/com/javatechie/spring/aop/api/controller/ProductController.java
+++ src/main/java/com/javatechie/spring/aop/api/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.javatechie.spring.aop.api.controller;
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.javatechie.spring.aop.api.model.Product;
 import com.javatechie.spring.aop.api.service.ProductService;
 
@@ -19,22 +17,15 @@ public class ProductController {
 	@Autowired
 	private ProductService service;
 
-
 	@PostMapping("/save")
 	public List<Product> saveProducts(@RequestBody List<Product> products) {
-
 		List<Product> pList = service.addProduct(products);
-
 		return pList;
 	}
 
 	@GetMapping("/findProducts")
 	public List<Product> getProducts() {
-		
 		List<Product> products = service.findAllProducts();
-		System.out.println("myget "+products.get(0).getName());
-	
 		return products;
 	}
-
 }
