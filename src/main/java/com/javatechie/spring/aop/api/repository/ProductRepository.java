7964152988f8cec408f43bd95f04f67b1a78293c
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2020-12-08 00:44:06 -0500

    Update ProductRepository.java

--- src/main/java/com/javatechie/spring/aop/api/repository/ProductRepository.java
+++ src/main/java/com/javatechie/spring/aop/api/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.javatechie.spring.aop.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.javatechie.spring.aop.api.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
